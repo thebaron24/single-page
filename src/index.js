a0109569525dec31b827f5f4b1c53ca29b0e1be1
bwilson <bwilson@bridge2solutions.com>
2017-10-11 09:55:03 -0400

    added latest theme layout and javascript

--- src/index.js
+++ src/index.js
@@ -1,26 +1,48 @@
 import styles from 'styles';
-import {MDCTemporaryDrawer, MDCTemporaryDrawerFoundation, util} from '@material/drawer';
+import * as mdc from 'material-components-web';
 import Template from './templates/template.handlebars';
+import config from './api/config.json';
+
 var app;
 
-const config = {};
-
 var AppComponent = () => {
-	app = document.createElement('div');
-	app.id = "app";
-	app.className = "mdc-typography";
-	app.innerHTML = Template(config);
-	document.body.className = "";
-	document.body.appendChild(app);
+  app = document.createElement('div');
+  app.id = "app";
+  app.className = "mdc-typography mdc-toolbar-fixed-adjust";
+  app.innerHTML = Template(config);
+  document.body.className = "";
+  document.body.appendChild(app);
 
-    let drawerEl = document.querySelector('.mdc-temporary-drawer');
-    let drawer = new MDCTemporaryDrawer(drawerEl);
-    let menu = document.querySelector('.menu');
-    menu.addEventListener('click', () => drawer.open = !drawer.open);
+  //material functions logged
+  console.log("MDC: ", mdc);
 
-    //event hooks
-    drawerEl.addEventListener('MDCTemporaryDrawer:open', () => console.log('Received MDCPersistentDrawer:open'));
-    drawerEl.addEventListener('MDCTemporaryDrawer:close', () => console.log('Received MDCPersistentDrawer:close'));
+
+  let drawerEl = document.querySelector('.mdc-temporary-drawer');
+  let drawer = new mdc.drawer.MDCTemporaryDrawer(drawerEl);
+  let menu = document.querySelector('.menu');
+  menu.addEventListener('click', () => drawer.open = !drawer.open);
+
+  //event hooks
+  drawerEl.addEventListener('MDCTemporaryDrawer:open', () => console.log('Received MDCPersistentDrawer:open'));
+  drawerEl.addEventListener('MDCTemporaryDrawer:close', () => console.log('Received MDCPersistentDrawer:close'));
+
+  var pollId = 0;
+  pollId = setInterval(function() {
+    var pos = getComputedStyle(document.querySelector('.mdc-toolbar')).position;
+    if (pos === 'fixed' || pos === 'relative') {
+      init();
+      clearInterval(pollId);
+    }
+  }, 250);
+
+  function init() {
+    var toolbar = mdc.toolbar.MDCToolbar.attachTo(document.querySelector('.mdc-toolbar'));
+    toolbar.listen('MDCToolbar:change', function(evt) {
+      var flexibleExpansionRatio = evt.detail.flexibleExpansionRatio;
+    });
+    toolbar.fixedAdjustElement = document.querySelector('.mdc-toolbar-fixed-adjust');
+  }
+  
 }
 
 AppComponent();
